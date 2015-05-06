7dec6ddbfd2f4cf650c430fbf12338c35746732c
softcactus <cactus@cactusworks.net>
2015-05-06 15:52:24 -0700

    now displays background streetview of address given

--- js/script.js
+++ js/script.js
@@ -6,16 +6,29 @@ function loadData() {
     var $nytHeaderElem = $('#nytimes-header');
     var $nytElem = $('#nytimes-articles');
     var $greeting = $('#greeting');
+    var $bodycontainer = $('#bodycontainer')
 
     // clear out old data before new request
     $wikiElem.text("");
     $nytElem.text("");
 
     // load streetview
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
+    var address = streetStr + ', ' + cityStr;
 
+    var streetviewURL = "https://maps.googleapis.com/maps/api/streetview?size=640x400&location=" + address;
+    $(bodycontainer).css('background-image', 'url("'+ streetviewURL + '")');
     // YOUR CODE GOES HERE!
 
     return false;
 };
 
 $('#form-container').submit(loadData);
+
+
+
+// $('#divID').css("background-image", "url(/myimage.jpg)");  
+// 
+
+//    $bodycontainer.append('<img class="bgimg" src="' + streetviewURL + '">');
